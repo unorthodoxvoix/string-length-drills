5a96d8f2356919c6f984b3f3e64e0db800f278e0
unorthodoxvoix <75649701+unorthodoxvoix@users.noreply.github.com>
2021-03-27 16:08:04 -0400

    Add files via upload

---
+++ string-length-drills/string-length-drills.js
@@ -0,0 +1,64 @@
+//Please complete the string length drills. 
+//Test your answers on your borwser's console.
+//write the results in a js comment in this file. Submit when finished.
+
+//Example Drill Question
+//write a variable and call it str and have the value of "I love CodeSquad!", use the .length method to determine the length of the string value. 
+
+//Code Answer of Example Question Drill
+let str = "I love CodeSquad!";
+// console.log(str.length);
+
+//Result Answer: 17
+
+/*____________________________________________*/
+/*____________________________________________*/
+/*____________________________________________*/
+
+//1
+//write a variable and call it boba and have the value of "By Boba, I think they get it!", use the .length method to determine the length of the string value. 
+
+//1 Code Answer
+
+let boba = "By Boba, I think they get it!";
+console.log(boba.length);
+
+//1 Result Answer: 29
+
+//2
+//write a variable and call it dogQuestion and have the value of "But what if I like dogs more than cats?", use the .length method to determine the length of the string value. 
+
+//2 Code Answer
+
+let dogQuestion = "But what if I like dogs more than cats?";
+console.log(dogQuestion.length);
+
+//2 Result Answer: 39
+
+//3
+//write a variable and call it name and have the value be your name, use the .length method to determine the length of the string value. 
+
+//3 Code Answer
+
+let name = "Quentin";
+console.log(name.length);
+
+//3 Result Answer: 7
+
+//4
+//write a variable and call it city and have the value of the name of your favorite city or town, use the .length method to determine the length of the string value. 
+
+//4 Code Answer
+let city = "Los Angeles";
+console.log(city.length);
+
+//4 Result Answer: 11
+
+//5
+//write a variable and call it noms and have the value of the name any type of snack you like, use the .length method to determine the length of the string value. 
+
+//5 Code Answer
+let noms = "cake";
+console.log(noms.length);
+
+//5 Result Answer: 4
